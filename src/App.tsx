bc94faa340ba6354c143fd1dbb6f64fc5b1d16bd
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-17 01:57:54 +1000

    Build accordion that can be opened one item at a time.
    did not do the animation -> slowly open part yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+const daddyJokes = [
+  {
+    question: "Why don't skeletons fight each other?",
+    answer: "They don't have the guts.",
+  },
+  {
+    question: "What do you call a fake noodle?",
+    answer: "An impasta.",
+  },
+  {
+    question: "Why did the scarecrow win an award?",
+    answer: "Because he was outstanding in his field.",
+  },
+];
 function App() {
+  const [activeIndex, setActiveIndex] = useState<number | undefined>();
   return (
     <>
-      <p>Accordion</p>
+      <h1>Accordion for Daddy Jokes</h1>
+
+      <h2>Only one can be opened</h2>
+      <div className="accordion-items">
+        {daddyJokes.map(({ question, answer }, index) => (
+          <div key={question}>
+            <div
+              className="accordion-title"
+              onClick={() => setActiveIndex(index)}
+            >
+              <div>{question}</div>
+              <div>+</div>
+            </div>
+            {activeIndex === index && (
+              <div className="accordion-content">{answer}</div>
+            )}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
